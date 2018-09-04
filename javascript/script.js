b81f4dd9569c02087a93f78003d169fbadfe5ef5
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2018-09-04 13:57:31 +0200

    width media queries done

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,35 @@
-const left = document.querySelector('.bigBox__left');
-const right = document.querySelector('.bigBox__right');
 
-left.addEventListener("mouseenter", () => {
-    left.classList.add("hover");
-    right.classList.add("smaller");
-});
+formAnimation = () => {
+    let screen =  window.innerWidth;
+    const left = document.querySelector('.bigBox__left');
+    const right = document.querySelector('.bigBox__right');
 
-left.addEventListener("mouseleave", () => {
-    left.classList.remove("hover");
-    right.classList.remove("smaller");
-});
+    if(screen >= 600) {
+        left.addEventListener("mouseenter", () => {
+            left.classList.add("hover");
+            right.classList.add("smaller");
+        });
+        
+        left.addEventListener("mouseleave", () => {
+            left.classList.remove("hover");
+            right.classList.remove("smaller");
+        });
+        
+        right.addEventListener("mouseenter", () => {
+            right.classList.add("hover");
+            left.classList.add("smaller");
+        });
+        
+        right.addEventListener("mouseleave", () => {
+            right.classList.remove("hover");
+            left.classList.remove("smaller");
+        });
+    }
 
-right.addEventListener("mouseenter", () => {
-    right.classList.add("hover");
-    left.classList.add("smaller");
-});
+    console.log(screen);
+};
 
-right.addEventListener("mouseleave", () => {
-    right.classList.remove("hover");
-    left.classList.remove("smaller");
-});
+window.addEventListener("resize", formAnimation);
+
+
+
