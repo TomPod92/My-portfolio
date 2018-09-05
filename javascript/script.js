8888e9f177fb088e550e5688e043ac7ed8eb40a8
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2018-09-05 13:07:06 +0200

    media queries done

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,9 @@
+const left = document.querySelector('.bigBox__left');
+const right = document.querySelector('.bigBox__right');
+
 
 formAnimation = () => {
     let screen =  window.innerWidth;
-    const left = document.querySelector('.bigBox__left');
-    const right = document.querySelector('.bigBox__right');
 
     if(screen >= 600) {
         left.addEventListener("mouseenter", () => {
@@ -24,12 +25,28 @@ formAnimation = () => {
             right.classList.remove("hover");
             left.classList.remove("smaller");
         });
-    }
+    } else removeClass();
 
     console.log(screen);
 };
 
 window.addEventListener("resize", formAnimation);
 
+removeClass = ()=> {
 
+    console.log('class removed');
+    left.classList.remove("hover");
+    right.classList.remove("smaller");
+
+    right.classList.remove("hover");
+    left.classList.remove("smaller");
+};
+
+
+
+// // rotate project card after clicking on a button
+
+// document.querySelector('.project__card--rotate-button').addEventListener('click', () => {
+//     document.querySelector('.project__card--front').
+// });
 
